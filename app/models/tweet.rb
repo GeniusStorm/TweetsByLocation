0da4e355f1d5bc14fe8d8f37f5ef490dc766d0ca
Raghu Kamat <rgkamat21@gmail.com>
2014-03-24 08:46:46 -0700

    Updating specs and add a validation for allowing entering point based coordinates only
    Allowing only point based coordinates

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -14,12 +14,21 @@ class Tweet
        Tweet.where(:location.near => [longitude, latitude]).limit(50).to_a
 	end
 
-	def valid_coordinates? 
-		if [longitude, latitude].map(&:present?).include?(false)
-			errors.add(:base , "Latitude/Longitude must be present") 
+	def valid_coordinates?
+		if [longitude, latitude].map(&:present?).include?(false) || coordinates_are_integer?
+			errors.add(:base , "The coordinates are invalid") 
 	    	return false
-		else
-			return true
-		end	
+	    else
+	    	return true
+		end
+			
+	end
+
+	def coordinates_are_integer?
+		state =[]
+		[latitude,longitude].each do |coordinate|
+    	 state <<  (coordinate.match(/\A[+-]?\d+?(\.\d+)?\Z/) == nil)
+    	end
+    	return state.include?(true)
 	end
 end
--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -7,12 +7,10 @@ describe Tweet do
 		before(:each) do
 			# Mongoid.purge! 
 			TweetLocator.fetch_and_save_tweets #adding tweets to the test database
-			@longitude = ""
-			@latitude = "90"
 		end
 
 		it "gets the tweets given the coordinates" do
-			@tweet = Tweet.new(:latitude => @latitude , :longitude => @longitude)
+			@tweet = Tweet.new(:latitude => "-180" , :longitude => "90")
 			@tweets =  @tweet.get_closest_tweets
 			@tweets.should_not be_empty
 			@tweets.first.should be_kind_of Tweet
@@ -22,7 +20,7 @@ describe Tweet do
 			@tweet = Tweet.new(:latitude => "" , :longitude => "")
 			@tweet.valid_coordinates?.should be_false
 			@tweet.errors.full_messages.should be_kind_of(Array)
-			@tweet.errors.full_messages.first.should == "Latitude/Longitude must be present"
+			@tweet.errors.full_messages.first.should == "The coordinates are invalid"
 		end
 
 
@@ -30,6 +28,12 @@ describe Tweet do
 			@tweet = Tweet.new(:latitude => "-20" , :longitude => "40.5")
 			@tweet.valid_coordinates?.should be_true
 			@tweet.errors.full_messages.should be_empty
+		end
+
+		it "retuns false with invalid coordinates" do
+			@tweet = Tweet.new(:latitude => "-20,33.6" , :longitude => "40.5,45.5")
+			@tweet.valid_coordinates?.should be_false
+			@tweet.errors.full_messages.first.should == "The coordinates are invalid"
 		end
 	end
 end
