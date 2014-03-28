72eb1692a3292661cb080494e7d3163b1c5e09d6
Raghu Kamat <rgkamat21@gmail.com>
2014-03-28 02:14:45 -0700

    Fixing fiber implementation

--- lib/tweet_locator.rb
+++ lib/tweet_locator.rb
@@ -7,23 +7,18 @@ class TweetLocator
 	#get tweets
 	def self.fetch_and_save_tweets
 		EventMachine.run do #using eventmachine on tweet insert
-
+			Fiber.new {
 			client = TweetStream::Client.new
-			@tweet_fibers = [Fiber.current]
-			@@tweet_count = 0
 			client.locations(-180, -90, 180, 90) do |tweet|
-				return if @@tweet_count ==  50
-				@tweet_fibers << Fiber.new do
-					self.save_tweet_to_db(tweet)
-					@@tweet_count += 1
-				end
-				@tweet_fibers.last.resume
+				self.save_tweet_to_db(tweet)		
 			end
+			}.resume
 		end #end of eventmachine run
 	end
 
 	#save tweet
 	def self.save_tweet_to_db(tweet)
+		f = Fiber.current
 		if tweet['geo']
 			Tweet.create!({
 				location: tweet['geo']['coordinates'],
@@ -31,7 +26,9 @@ class TweetLocator
 			    profile_pic_url:  tweet['user']['profile_image_url'],
 				user_handle: tweet['user']['screen_name']
 			})
-		end	
+		end
+		return Fiber.yield
+		f.resume	
 	end
 
 	#fetch trending tweets by location and trends
